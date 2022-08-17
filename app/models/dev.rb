08c7e44bd6913f9d195795c69ecd516fca580cb0
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-17 09:20:24 -0500

    All deliverables complete

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.any? { |freebie| freebie.item_name == item_name }
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev == self
+      freebie.update(dev: dev)
+      "#{freebie.item_name} has been given to #{dev.name}"
+    else
+      "That freebie doesn't belong to #{dev.name}"
+    end
+  end
 
 end
